fc33f3717ea9ce87d50ff6eca46e01b10e0b57d4
Ting <ootuskaai@gmail.com>
2020-03-04 11:08:16 +0800

    implement methods
    implement some methods about min() sum() and so on...

--- src/com/company/ArrayTing.java
+++ src/com/company/ArrayTing.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class ArrayTing {
     private int[] items;
     private int count;
 
-    public ArrayTing(int length) {
-        items = new int[length];
+    public ArrayTing(int len) {
+        items = new int[len];
     }
 
     public void insert(int item) {
@@ -47,11 +49,6 @@ public class ArrayTing {
         }
     }
 
-    public void print() {
-        for(int i = 0 ; i < count; i++)
-            System.out.println(items[i]);
-    }
-
     public int indexOf(int item) {
         for(int i = 0; i < count; i++)
             if(items[i] == item)
@@ -60,7 +57,7 @@ public class ArrayTing {
     }
 
     public int max() {
-        int max = 0;
+        int max = items[0];
         for (int item:items)
             if (item > max)
                 max = item;
@@ -69,26 +66,41 @@ public class ArrayTing {
     }
 
     public int min() {
-        return 0;
+        int min = items[0];
+        for(int item:items)
+            if(item < min)
+                min = item;
+        return min;
     }
 
     public int sum() {
-        return 0;
+        int sum = 0;
+        for (int item:items)
+            sum += item;
+        return sum;
     }
 
-    public int countItem(int item) {
-        return 0;
+    public int countByItem(int item) {
+        int countItem = 0;
+        for (int i:items)
+            if(i == item)
+                countItem++;
+        return countItem;
     }
 
     public int firstItem() {
-        return 0;
+        return items[0];
     }
 
     public int lastItem() {
-        return 0;
+        return items[count - 1];
     }
 
-    public int count() {
+    public int lastItemIndex() {
+        return count - 1;
+    }
+
+    public int curCount() {
         return count;
     }
 
@@ -112,5 +124,22 @@ public class ArrayTing {
             if(other.contains(item))
                 intersect.insert(item);
         return intersect;
+    }
+
+    public ArrayTing notIntersect(ArrayTing other) {
+        ArrayTing notIntersect = new ArrayTing(count);
+        for (int item: items)
+            if(!other.contains(item))
+                notIntersect.insert(item);
+        return notIntersect;
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(items));
+    }
+
+    public void printEach() {
+        for(int item:items)
+            System.out.println(item);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
         ArrayTing numbers = new ArrayTing(3);
         numbers.insert(100);
         numbers.insert(200);
-        numbers.insert(300);
-        numbers.reverse();
+        numbers.insert(100);
+        numbers.insert(200);
+        numbers.insert(100);
         numbers.print();
-
     }
 }
