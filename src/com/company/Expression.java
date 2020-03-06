069feaa6e6409073cd826b5dc6e24d2498a30f3d
Ting <ootuskaai@gmail.com>
2020-03-06 11:54:44 +0800

    update expression

--- src/com/company/Expression.java
+++ src/com/company/Expression.java
@@ -1,13 +1,47 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
 
+    private final List leftBracket = Arrays.asList('(', '{', '[','<');
+    private final List rightBracket = Arrays.asList(')', '}', ']', '>');
 
+    public boolean isBalance(String input){
+        if(input == null)
+            throw new IllegalArgumentException();
+
+        Stack<Character> stack = new Stack<>();
+        for (char ch: input.toCharArray()) {
+            if(isLeftBracket(ch)) stack.push(ch);
+
+            if (isRightBracket(ch)) {
+                if (stack.empty()) return false;
+
+                char top = stack.pop();
+                if (bracketMatch(top,ch)) return false;
+            }
+        }
+
+        return stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return leftBracket.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBracket.contains(ch);
+    }
+
+    private boolean bracketMatch(char left, char right) {
+        return leftBracket.indexOf(left) == rightBracket.indexOf(right);
+    }
 
     //version 2 ting
-    public boolean balanced2(String input) {
+    public boolean isBalanced2(String input) {
         if (input == null)
             throw new IllegalArgumentException();
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,10 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
+        //Edge cases
+        Expression exp = new Expression();
+        String str = "(1 + 2)";
+        boolean b = exp.isBalance(str);
+        System.out.println(b);
     }
 }
