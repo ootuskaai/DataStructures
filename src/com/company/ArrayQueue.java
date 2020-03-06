ea1d74304560e3cbd78233efc2f9805a6e4d9ae2
Ting <ootuskaai@gmail.com>
2020-03-06 18:04:31 +0800

    implement ArrayQueue class :+1:
    implement ArrayQueue class :+1:

--- src/com/company/ArrayQueue.java
+++ src/com/company/ArrayQueue.java
@@ -1,4 +1,41 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class ArrayQueue {
+    //ArrayQueue (ArrayDeque)
+
+    private int[] items;
+    private int rear;
+    private int front;
+    private int count;
+
+    public ArrayQueue(int capacity) {
+        items = new int[capacity];
+    }
+
+    public void enqueue(int item) {
+        if(count == items.length)
+            throw new IllegalStateException("capacity full");
+
+        items[rear] = item;
+        rear = (rear + 1) % items.length;
+        count++;
+    }
+
+    public int dequeue() {
+        if(count == 0)
+            throw new IllegalStateException();
+
+        int item = items[front];
+        items[front] = 0;
+        front = (front + 1) % items.length;
+        count--;
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
 }
