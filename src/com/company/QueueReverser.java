a4bcb99df2aadf5f6810d6d45340430e06fdba03
Ting <ootuskaai@gmail.com>
2020-03-08 13:55:32 +0800

    add class QueueReverser and implement :+1:

---
+++ src/com/company/QueueReverser.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueReverser {
+
+    public static void reverse(Queue<Integer> queue, int k) {
+        if(k < 0 || k > queue.size())
+            throw new IllegalArgumentException();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k;i++)
+            stack.push(queue.remove());
+
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
+
+        for (int i = 0;i < queue.size() - k; i++)
+            queue.add(queue.remove());
+    }
+}
